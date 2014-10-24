875d5e1622bc4c78f9e7e795c1320ac7b9a920c2
Chrisitan Gao <flyingabove@gmail.com>
2014-10-24 16:16:33 -0700

    new stuff

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Portfolio</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--[if lte IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
+<script type="text/javascript" src="js/jquery-1.2.3.pack.js"></script>
+<script type="text/javascript" src="js/jquery.jcarousel.pack.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function() {
+	jQuery('#mycarousel').jcarousel({
+		scroll: 1,
+		animation: "slow",
+		wrap: "both"
+	});
+});
+</script>
+</head>
+<body>
+<!-- Header -->
+<div id="header">
+  <div class="shell">
+    <div class="cl">&nbsp;</div>
+    <!-- Logo -->
+    <div id="logo-cnt">
+      <h1 id="logo">Personal Site Name</h1>
+      <h2 id="slogan">yourname personal portfolio</h2>
+    </div>
+    <!-- End Logo -->
+    <div id="status">
+      <p class="available">Login &nbsp;&nbsp;&nbsp;&nbsp; Sign Up</p>
+    </div>
+    <div class="cl">&nbsp;</div>
+    <!-- Carousel -->
+    <div id="slider">
+      <ul id="mycarousel">
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-1.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="optimizationOne.php"><img src="css/images/port-2.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-3.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-4.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-5.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-6.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-6.gif" alt="" /> </a> </div>
+        </li>
+        <li>
+          <div class="img-cnt"> <a href="index.php"><img src="css/images/port-6.gif" alt="" /> </a> </div>
+        </li>
+      </ul>
+    </div>
+    <!-- End Carousel -->
+  </div>
+</div>
+<!-- End Header -->
+<!-- Main -->
+<div id="main">
+  <div class="shell">
+    <!-- Article -->
+    <div class="article">
+      <h2>Triple Exponential Demo</h2>
+      <iframe height="1000" width="100%" frameBorder="0" src="https://christiangao.shinyapps.io/HoltFive/">your browser does not support IFRAMEs</iframe>
+      <!-- 
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a urna sit amet leo sagittis cursus a at lectus. Donec <a href="index.php">bibendum facilisis ultricies</a>. Maecenas nec arcu dui, ut lobortis erat. In nec condimentum quam. Vivamus euismod scelerisque ligula vitae fermentum. Nulla dignissim rutrum eleifend.</p>
+   	  -->
+    </div>
+    <!-- End Article -->
+    <!-- Gallery -->
+    
+    <!-- End Contact -->
+  </div>
+</div>
+<!-- End Main -->
+<div id="footer">
+  <p>&copy; ConsultGao.com.</p>
+</div>
+
+</html>
